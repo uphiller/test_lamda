1aca2ebf95cd31e59c5ee75585d16c266b8e3c69
jason <jason.uphiller@gmail.com>
2018-04-02 16:10:55 +0900

    add

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 var jwt = require('jsonwebtoken');
 exports.handler = function(event, context,callback) {
-  const token = event.headers['Authorization'];
-
-
+  var token = event.authorizationToken;
   jwt.verify(token, 'shhhhh', function(err, decoded) {
     if (err) {
-      callback(null, {"statusCode": 401, "body": err.message});
+      callback("Unauthorized");
     }else{
-      callback(null, {"statusCode": 200, "body": token});
+      callback(null, generatePolicy('user', 'Allow', event.methodArn));
     }
   });
+}
+
+// Help function to generate an IAM policy
+var generatePolicy = function(principalId, effect, resource) {
+  var authResponse = {};
+
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    var policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+
+  return authResponse;
 }
 
