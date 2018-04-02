bf12040b439673d8be8d6ee75412a7893d53e19a
jason <jason.uphiller@gmail.com>
2018-04-02 13:47:14 +0900

    add

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var jwt = require('jsonwebtoken');
-exports.handler = function(event, context) {
-  callback(null, {"statusCode": 200, "body": "ok!!"});
+exports.handler = function(event, context,callback) {
+  const token = event.headers['Authorization'];
+
+
+  jwt.verify(token, 'shhhhh', function(err, decoded) {
+    if (err) {
+      callback(null, {"statusCode": 200, "body": err.message});
+    }else{
+      callback(null, {"statusCode": 200, "body": token});
+    }
+  });
 }
 
