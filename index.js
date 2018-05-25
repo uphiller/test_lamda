f349f49673825fe0ed14f4e72efc52ca3cd1a9fc
jason <jason.uphiller@gmail.com>
2018-05-25 14:24:38 +0900

    add npm mysql

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var jwt = require('jsonwebtoken');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "mealcoupon-release-cluster.cluster-ro-cofqettussu1.ap-northeast-2.rds.amazonaws.com",
+  user: "root",
+  password: "tnavhdlsxmslave",
+  database: "mysql",
+});
+// console.log(connection);
+exports.handler = (event, context, callback) => {
+  connection.query('SELECT * FROM  mysql.slow_log order by start_time desc limit 1, 10', function (error, results, fields) {
+    if (error) {
+      connection.destroy();
+      throw error;
+    } else {
+      // connected!
+      console.log(results);
+      callback(error, results);
+      connection.end(function (err) { callback(err, results);});
+    }
+  });
+};
+/*var jwt = require('jsonwebtoken');
 exports.handler = function(event, context,callback) {
   var token = event.authorizationToken;
   jwt.verify(token, 'shhhhh', function(err, decoded) {
@@ -28,5 +49,5 @@ var generatePolicy = function(principalId, effect, resource) {
   }
 
   return authResponse;
-}
+}*/
 
